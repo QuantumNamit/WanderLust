58c25601554572d41224a23ae70d0ac3d30b07aa
Namit Aneja <anejanamit1@gmail.com>
2025-05-18 19:53:06 +0530

    changed few features

--- init/data.js
+++ init/data.js
@@ -10,6 +10,7 @@ const sampleListings = [
     price: 1500,
     location: "Malibu",
     country: "United States",
+    geometry: { type: 'Point', coordinates: [118.6923, 34.0381] } // longitude,latitude
   },
   {
     title: "Modern Loft in Downtown",
@@ -22,6 +23,7 @@ const sampleListings = [
     price: 1200,
     location: "New York City",
     country: "United States",
+    geometry: { type: 'Point', coordinates: [74.0060, 40.7128] } // longitude,latitude
   },
   {
     title: "Mountain Retreat",
@@ -34,6 +36,7 @@ const sampleListings = [
     price: 1000,
     location: "Aspen",
     country: "United States",
+    geometry: { type: 'Point', coordinates: [106.8175, 39.1911] } // longitude,latitude
   },
   {
     title: "Historic Villa in Tuscany",
@@ -46,6 +49,7 @@ const sampleListings = [
     price: 2500,
     location: "Florence",
     country: "Italy",
+    geometry: { type: 'Point', coordinates: [11.2577, 43.7700] } // longitude,latitude
   },
   {
     title: "Secluded Treehouse Getaway",
@@ -58,6 +62,7 @@ const sampleListings = [
     price: 800,
     location: "Portland",
     country: "United States",
+    geometry: { type: 'Point', coordinates: [122.6784, 45.5152] } // longitude,latitude
   },
   {
     title: "Beachfront Paradise",
@@ -70,6 +75,7 @@ const sampleListings = [
     price: 2000,
     location: "Cancun",
     country: "Mexico",
+    geometry: { type: 'Point', coordinates: [86.8515, 21.1619] } // longitude,latitude
   },
   {
     title: "Rustic Cabin by the Lake",
@@ -82,6 +88,7 @@ const sampleListings = [
     price: 900,
     location: "Lake Tahoe",
     country: "United States",
+    geometry: { type: 'Point', coordinates: [120.0324, 39.0968] } // longitude,latitude
   },
   {
     title: "Luxury Penthouse with City Views",
@@ -94,6 +101,7 @@ const sampleListings = [
     price: 3500,
     location: "Los Angeles",
     country: "United States",
+    geometry: { type: 'Point', coordinates: [118.2426, 34.0549] } // longitude,latitude
   },
   {
     title: "Ski-In/Ski-Out Chalet",
@@ -106,18 +114,7 @@ const sampleListings = [
     price: 3000,
     location: "Verbier",
     country: "Switzerland",
-  },
-  {
-    title: "Safari Lodge in the Serengeti",
-    description:
-      "Experience the thrill of the wild in a comfortable safari lodge. Witness the Great Migration up close.",
-    image: {
-      filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjl8fG1vdW50YWlufGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
-    },
-    price: 4000,
-    location: "Serengeti National Park",
-    country: "Tanzania",
+    geometry: { type: 'Point', coordinates: [7.2286, 46.0961] } // longitude,latitude
   },
   {
     title: "Historic Canal House",
@@ -130,6 +127,7 @@ const sampleListings = [
     price: 1800,
     location: "Amsterdam",
     country: "Netherlands",
+    geometry: { type: 'Point', coordinates: [4.9041, 52.3676] } // longitude,latitude
   },
   {
     title: "Private Island Retreat",
@@ -142,6 +140,7 @@ const sampleListings = [
     price: 10000,
     location: "Fiji",
     country: "Fiji",
+    geometry: { type: 'Point', coordinates: [178.0650, 17.7134] } // longitude,latitude
   },
   {
     title: "Charming Cottage in the Cotswolds",
@@ -154,6 +153,7 @@ const sampleListings = [
     price: 1200,
     location: "Cotswolds",
     country: "United Kingdom",
+    geometry: { type: 'Point', coordinates: [1.8839, 51.7950] } // longitude,latitude
   },
   {
     title: "Historic Brownstone in Boston",
@@ -166,6 +166,7 @@ const sampleListings = [
     price: 2200,
     location: "Boston",
     country: "United States",
+    geometry: { type: 'Point', coordinates: [71.0565, 42.3555] } // longitude,latitude
   },
   {
     title: "Beachfront Bungalow in Bali",
@@ -178,6 +179,7 @@ const sampleListings = [
     price: 1800,
     location: "Bali",
     country: "Indonesia",
+    geometry: { type: 'Point', coordinates: [115.1889, 8.4095] } // longitude,latitude
   },
   {
     title: "Mountain View Cabin in Banff",
@@ -190,6 +192,7 @@ const sampleListings = [
     price: 1500,
     location: "Banff",
     country: "Canada",
+    geometry: { type: 'Point', coordinates: [115.5708, 51.1784] } // longitude,latitude
   },
   {
     title: "Art Deco Apartment in Miami",
@@ -202,6 +205,7 @@ const sampleListings = [
     price: 1600,
     location: "Miami",
     country: "United States",
+    geometry: { type: 'Point', coordinates: [80.1918, 25.7617] } // longitude,latitude
   },
   {
     title: "Tropical Villa in Phuket",
@@ -214,6 +218,7 @@ const sampleListings = [
     price: 3000,
     location: "Phuket",
     country: "Thailand",
+    geometry: { type: 'Point', coordinates: [98.3923, 7.8804] } // longitude,latitude
   },
   {
     title: "Historic Castle in Scotland",
@@ -226,6 +231,7 @@ const sampleListings = [
     price: 4000,
     location: "Scottish Highlands",
     country: "United Kingdom",
+    geometry: { type: 'Point', coordinates: [5.0000, 57.6667] } // longitude,latitude
   },
   {
     title: "Desert Oasis in Dubai",
@@ -238,6 +244,7 @@ const sampleListings = [
     price: 5000,
     location: "Dubai",
     country: "United Arab Emirates",
+    geometry: { type: 'Point', coordinates: [55.2708, 25.2048] } // longitude,latitude
   },
   {
     title: "Rustic Log Cabin in Montana",
@@ -250,6 +257,7 @@ const sampleListings = [
     price: 1100,
     location: "Montana",
     country: "United States",
+    geometry: { type: 'Point', coordinates: [110.3626, 46.8797] } // longitude,latitude
   },
   {
     title: "Beachfront Villa in Greece",
@@ -262,6 +270,7 @@ const sampleListings = [
     price: 2500,
     location: "Mykonos",
     country: "Greece",
+    geometry: { type: 'Point', coordinates: [25.3667, 37.4415] } // longitude,latitude
   },
   {
     title: "Eco-Friendly Treehouse Retreat",
@@ -274,6 +283,7 @@ const sampleListings = [
     price: 750,
     location: "Costa Rica",
     country: "Costa Rica",
+    geometry: { type: 'Point', coordinates: [83.7534, 9.7489] } // longitude,latitude
   },
   {
     title: "Historic Cottage in Charleston",
@@ -286,6 +296,7 @@ const sampleListings = [
     price: 1600,
     location: "Charleston",
     country: "United States",
+    geometry: { type: 'Point', coordinates: [79.9320, 32.7833] } // longitude,latitude
   },
   {
     title: "Modern Apartment in Tokyo",
@@ -298,6 +309,7 @@ const sampleListings = [
     price: 2000,
     location: "Tokyo",
     country: "Japan",
+    geometry: { type: 'Point', coordinates: [139.6500, 35.6764] } // longitude,latitude
   },
   {
     title: "Lakefront Cabin in New Hampshire",
@@ -310,6 +322,7 @@ const sampleListings = [
     price: 1200,
     location: "New Hampshire",
     country: "United States",
+    geometry: { type: 'Point', coordinates: [71.5724, 43.1939] } // longitude,latitude
   },
   {
     title: "Luxury Villa in the Maldives",
@@ -322,6 +335,7 @@ const sampleListings = [
     price: 6000,
     location: "Maldives",
     country: "Maldives",
+    geometry: { type: 'Point', coordinates: [73.2207, 3.2028] } // longitude,latitude
   },
   {
     title: "Ski Chalet in Aspen",
@@ -334,6 +348,7 @@ const sampleListings = [
     price: 4000,
     location: "Aspen",
     country: "United States",
+    geometry: { type: 'Point', coordinates: [106.8175, 39.1911] } // longitude,latitude
   },
   {
     title: "Secluded Beach House in Costa Rica",
@@ -346,6 +361,7 @@ const sampleListings = [
     price: 1800,
     location: "Costa Rica",
     country: "Costa Rica",
+    geometry: { type: 'Point', coordinates: [83.7534, 9.7489] } // longitude,latitude
   },
 ];
 
--- init/index.js
+++ init/index.js
@@ -11,12 +11,12 @@ main().then(()=>{
 })
 
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+    await mongoose.connect("mongodb+srv://Namit_Aneja:7Bv52MlPmXSYW8Vd@cluster0.cmlqkbe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 }
 
 const initDB=async()=>{
     await Listing.deleteMany({});
-    initData.data=initData.data.map((obj)=>({...obj,owner:"6825ddf8852e44a7eeff0db7"}))
+    initData.data=initData.data.map((obj)=>({...obj,owner:"6829e30f4791a1e6f62eb813"}))
     await Listing.insertMany(initData.data);
     console.log("data was initalised")
 }
